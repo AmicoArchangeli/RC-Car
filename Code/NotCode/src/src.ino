b739854688512c8f7b8cccee75b8beb33884115a
mp2223 <miles.petterson@yale.edu>
2016-10-22 17:05:36 -0400

    Added servo library
    Descriptive Description

--- Code/NotCode/src/src.ino
+++ Code/NotCode/src/src.ino
@@ -1,4 +1,5 @@
 #include <PS2X_lib.h>
+#include <Servo.h>
 
 // PS2 PIN SETTINGS
 int CLK_PIN = 13;
@@ -10,7 +11,10 @@ bool RMBLE_ON = true;
 
 // MOTOR + SERVO PIN MAPPINGS
 int MTR_PIN = 3;
-int SRV_PIN = 5;
+int SRV_PIN = 9;
+
+int SRV_CENTER = 90;
+int SRV_MAX_OFFSET = 80;
 
 // Communication details
 int SERIAL_RATE = 9600;
@@ -19,12 +23,15 @@ int PS2X_TYPE = 0;
 // PS2X controller instances
 PS2X ps2x;
 
+// Servo instance
+Servo servo;
+
 void setup() {
   // Rate at which serial will monitor
   Serial.begin(SERIAL_RATE);
 
   // Attempt configuration and shit
-  int error = ps2x.config_gamepad(13, 11, 10, 12, PRESS_ON, RMBLE_ON);
+  int error = ps2x.config_gamepad(CLK_PIN, CMD_PIN, ATN_PIN, DAT_PIN, PRESS_ON, RMBLE_ON);
 
   if (error == 1 || error == 2) {
     Serial.println("BITCH YOU FUCKING BRICKED THE CONTROLLER");
@@ -42,7 +49,9 @@ void setup() {
 
   // Setup the motors.
   pinMode(MTR_PIN, OUTPUT);
-  pinMode(SRV_PIN, OUTPUT);
+
+  // Attach the servo
+  servo.attach(SRV_PIN);
 }
 
 void loop() {
@@ -57,6 +66,16 @@ void loop() {
   int R_X = ps2x.Analog(PSS_RX);
   int R_Y = ps2x.Analog(PSS_RY);
 
+  // double throt = max(0, (127.0 - L_Y) / 127);
+  // double steer = 
+  
+  // Serial.println(L_Y);
+  // Serial.println(throt);
+  // Serial.println((127.0 - L_Y) / 127);
+
+
+
+  /*
   Serial.print("LX: [");
   Serial.print(L_X);
   Serial.print("] L_Y: [");
@@ -66,7 +85,9 @@ void loop() {
   Serial.print("] R_Y: [");
   Serial.print(R_Y);
   Serial.println("]");
+  */
 
   // Do some things with these
+  analogWrite(MTR_PIN, 128);
   
 }
